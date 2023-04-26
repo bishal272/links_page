c73a63e701b83bfd041b2263834f8db264a907ed
bishal272 <merkasin272@gmail.com>
2023-04-26 11:40:06 +0530

    #2

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,7 +2,7 @@ const Link = ({ url, title }) => {
   return (
     <div className="rounded-full">
       <a href={url} target="_blank" rel="noreferrer">
-        <div className="mt-3 bg-white rounded-full text-center py-3 px-24 text-lg min-w-max">
+        <div className="mt-3 bg-white rounded-full text-center py-3 px-20 text-lg min-w-max">
           {title}
         </div>
       </a>
