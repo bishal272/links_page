296b8f4394a68fadf8fcc3426dbddff4cafcdd39
bishal272 <merkasin272@gmail.com>
2023-04-26 11:34:16 +0530

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,18 @@ import Link from "./components/Link";
 
 function App() {
   return (
-    <main>
-      <div className="max-w-sm mx-auto my-36">
-        <div className="flex flex-col">
-          <h1 className="text-white text-center text-4xl font-bold">Projects</h1>
-          <br />
-          <Buttons />
-          <Link url={"https://fullstack-booking-app.vercel.app/"} title={"Booking App"} />
-          <Link url={"https://todo-app-merkasin.vercel.app/"} title={"Todo App"} />
-          <Link url={"https://tic-tac-toe-merkasin.vercel.app/"} title={"Tic Tac Toe game"} />
-          <Link url={"https://bishal272.github.io/Google-keep-clone/"} title={"Keeper"} />
-        </div>
+    <div className="flex justify-center items-center flex-col">
+      <div className="flex flex-col -mt-10">
+        <h1 className="text-white text-center text-4xl font-bold">Projects</h1>
+        <br />
+        <Buttons />
+        <Link url={"https://fullstack-booking-app.vercel.app/"} title={"Booking App"} />
+        <Link url={"https://todo-app-merkasin.vercel.app/"} title={"Todo App"} />
+        <Link url={"https://tic-tac-toe-merkasin.vercel.app/"} title={"Tic Tac Toe game"} />
+        <Link url={"https://bishal272.github.io/Google-keep-clone/"} title={"Keeper"} />
       </div>
-      <div className="text-white text-center bottom-0">&copy;merkasin</div>
-    </main>
+      <div className="text-white text-center w-screen absolute bottom-0">&copy;merkasin</div>
+    </div>
   );
 }
 
--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,7 +2,9 @@ const Link = ({ url, title }) => {
   return (
     <div className="rounded-full">
       <a href={url} target="_blank" rel="noreferrer">
-        <div className="mt-3 bg-white rounded-full text-center py-3 text-lg">{title}</div>
+        <div className="mt-3 bg-white rounded-full text-center py-3 px-24 text-lg min-w-max">
+          {title}
+        </div>
       </a>
     </div>
   );
